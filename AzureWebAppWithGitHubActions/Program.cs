3032060fc7feb3203851335b46b61bb6a25c4f2f
Bohdan Belei <bohdan.belei@outlook.com>
2023-10-29 01:43:00 +0300

    Add Test endpoint.

--- AzureWebAppWithGitHubActions/Program.cs
+++ AzureWebAppWithGitHubActions/Program.cs
@@ -10,4 +10,9 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.MapGet("/test", ()
+        => Results.Ok("This application has been successfully deployed to Azure Web App using GitHub Actions."))
+    .WithName("Test")
+    .WithOpenApi();
+
 await app.RunAsync();
